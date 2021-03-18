e140ee9c9e477650d5ca3609275e4f41d215aaeb
mishidemudong <liang.dongkai@163.com>
2021-03-18 10:01:55 +0800

    33333

---
+++ 不同遍历方式将二叉树展成链表.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 16:18:18 2021
+
+@author: liang
+"""
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        preorderList = []
+
+        def preorderTraversal(root: TreeNode):
+            if root:
+                preorderList.append(root)
+                preorderTraversal(root.left)
+                preorderTraversal(root.right)
+
+        preorderTraversal(root)
+
+        size = len(preorderList)
+        for i in range(1, size):
+            prev, curr = preorderList[i - 1], preorderList[i]
+            prev.left = None
+            prev.right = curr
+
+        return preorderList
