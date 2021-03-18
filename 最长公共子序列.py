433ec688ac5e37aaa2574a5e5e2ff480e0d20a64
mishidemudong <liang.dongkai@163.com>
2021-03-18 20:15:05 +0800

    5555

--- 最长公共子序列.py
+++ 最长公共子序列.py
@@ -61,3 +61,23 @@ def lcs_dp(s1, s2):
 s1 = "abcde"
 s2 = "acxe"
 print(lcs_dp(s1, s2))
+
+class Solution:
+    def longestCommonSubsequence( text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+
+        #最长公共子序列长度，dp的初始化要大一位。
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        for i in range(1,m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max([
+                                    dp[i-1][j],
+                                    dp[i][j-1]
+                                    ])
+
+        return dp[m][n]
